190c0c067585a67073c79e788a3385d9ef60d22d
Emil Lengman <emillen@kth.se>
2016-09-05 16:21:28 +0200

    added messages migration

---
+++ database/migrations/2016_09_05_141614_create_messages_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create("messages", function (Blueprint $table) {
+            $table->increments("id");
+            $table->integer("to")->unsigned();
+            $table->integer("from")->unsigned();
+            $table->string("text");
+        });
+
+        Schema::table("messages", function (Blueprint $table) {
+
+            $table->foreign("to")->references("id")->on("users");
+            $table->foreign("from")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
