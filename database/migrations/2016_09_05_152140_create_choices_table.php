3d55e88050dcce5301132ca2a800ddb292ee4578
Emil Lengman <emillen@kth.se>
2016-09-05 17:26:37 +0200

    add choices table migration

---
+++ database/migrations/2016_09_05_152140_create_choices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create("choices", function(Blueprint $table){
+            $table->integer("user")->unsigned();
+            $table->integer("alternative")->unsigned();
+            $table->primary(["user", "alternative"]);
+            $table->foreign("user")->references("id")->on("users");
+            $table->foreign("alternative")->references("id")->on("alternatives");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop("choices");
+    }
+}
