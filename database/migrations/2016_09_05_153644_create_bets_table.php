9e320a0c657f3362f4c28fe75d67e4345f6302a5
Emil Lengman <emillen@kth.se>
2016-09-05 17:44:08 +0200

    add users table migration

---
+++ database/migrations/2016_09_05_153644_create_bets_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create("bets", function(Blueprint $table){
+            $table->increments("id");
+            $table->longText("description");
+            $table->integer("upvoteScore")->unsigned();
+            $table->dateTime("finishes")->nullable();
+            $table->integer("type")->unsigned();
+            $table->integer("result")->unsigned()->nullable();
+            $table->integer("category")->unsigned();
+            $table->integer("owner")->unsigned();
+        });
+
+        Schema::table("bets", function(Blueprint $table){
+            $table->foreign("type")->references("id")->on("bet_types");
+            $table->foreign("category")->references("id")->on("categories");
+            $table->foreign("owner")->references("id")->on("users");
+            $table->foreign("result")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+
+        Schema::drop("bets");
+    }
+}
