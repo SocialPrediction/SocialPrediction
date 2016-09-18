8c309bb8f579f17bae94182cd7cf252e7d32079a
Emil Lengman <emillen@kth.se>
2016-09-18 18:05:29 +0200

    add comment models and relationships

---
+++ app/Models/BetComment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BetComment extends Model
+{
+    //
+    protected $timestamps = false;
+    protected $table = "bet_comments";
+
+    public function bet()
+    {
+
+        return $this->belongsTo('App\Models\Bet', 'bet');
+    }
+
+    public function comment()
+    {
+        return $this->belongsTo('App\Models\Comment', 'comment');
+
+    }
+}
