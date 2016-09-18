77f2e9add9b4af5f2418d0b1c0b555a7c62629b1
Emil Lengman <emillen@kth.se>
2016-09-18 14:05:18 +0200

    add usermute

---
+++ app/Models/UserMute.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserMute extends Model
+{
+    //
+    public $timestamps = false;
+
+    public function blocker(){
+
+        $this->belongsTo('App\Models\User', 'blocker');
+    }
+
+    public function blockee(){
+
+        $this->belongsTo('App\Model\User', 'blockee');
+    }
+}
