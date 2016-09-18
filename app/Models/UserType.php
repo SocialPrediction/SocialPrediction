a553475c1d7cf55ddfb6940f6975aa15db9eff98
Emil Lengman <emillen@kth.se>
2016-09-18 13:55:52 +0200

    add UserType model, and relationship with User

---
+++ app/Models/UserType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserType extends Model
+{
+    //
+    public $timestamps = false;
+
+    public function users(){
+
+        return $this->hasMany('App\Models\User', 'user_type');
+    }
+}
