7a3b0757c550d0612db97ca2e11bb27fdd26e51b
Emil Lengman <emillen@kth.se>
2016-09-22 18:02:27 +0200

    add a test for testing

---
+++ tests/Models/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setup()
+    {
+        parent::setUp();
+
+        factory(User::class, 10)->create();
+        factory(User::class)->create([
+            'name' => 'Abigail',
+        ]);
+        factory(User::class, 10)->create();
+    }
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function test()
+    {
+
+        $this->assertTrue(App\Models\User::find(11)->name == 'Abigail');
+
+    }
+}
