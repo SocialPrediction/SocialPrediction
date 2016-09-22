2b99f562e1b6d9c9a045c48679af3d18673e8602
Emil Lengman <emillen@kth.se>
2016-09-22 16:54:14 +0200

    add factory for Alternative model

--- database/factories/AlternativeFactory.php
+++ database/factories/AlternativeFactory.php
@@ -1,7 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: daseel
- * Date: 2016-09-18
- * Time: 23:04
- */
+
+$factory->define(App\UserMute::class, function (Faker\Generator $faker) {
+
+    return [
+
+        'description' => $faker->name
+    ];
+});
