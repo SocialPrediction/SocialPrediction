03d5fb60a06c2101dbdf8044b8f4e13b2364d15a
Emil Lengman <emillen@kth.se>
2016-09-22 17:14:15 +0200

    add factory for Bet model

--- database/factories/BetFactory.php
+++ database/factories/BetFactory.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: daseel
- * Date: 2016-09-18
- * Time: 23:04
- */
+
+$factory->define(App\Bet::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->name,
+        'description' => $faker->paragraph,
+        'upvoteScore' => $faker->numberBetween(-100, 500),
+        'finishes' => $faker->dateTimeThisYear,
+        'type' => function () {
+            return factory(App\BetType::class)->create()->id;
+        },
+        'result' => function () {
+            if (rand(0, 10) % 2)
+                return null;
+
+            return factory(App\Alternative::class)->create()->id;
+        },
+        'category' => function () {
+            return factory(App\Category::class)->create()->id;
+        },
+        'owner' => function () {
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
