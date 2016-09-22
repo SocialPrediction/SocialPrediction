270abeafc150e8ad1acff0917b3480ae4edf5366
Emil Lengman <emillen@kth.se>
2016-09-22 16:47:24 +0200

    add factory for UserMute model

--- database/factories/UserMuteFactory.php
+++ database/factories/UserMuteFactory.php
@@ -5,3 +5,14 @@
  * Date: 2016-09-18
  * Time: 23:04
  */
+$factory->define(App\UserMute::class, function (Faker\Generator $faker) {
+
+    return [
+        'blocker' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'blockee' => function () {
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
