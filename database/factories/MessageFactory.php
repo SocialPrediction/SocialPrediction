fde1209b85398c5fc907ff999f8bf9dde57f865e
Emil Lengman <emillen@kth.se>
2016-09-22 16:51:03 +0200

    add factory for Message model

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -5,3 +5,16 @@
  * Date: 2016-09-18
  * Time: 23:04
  */
+$factory->define(App\UserMute::class, function (Faker\Generator $faker) {
+
+    return [
+        'to' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'from' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+
+        'text' => $faker->paragraph
+    ];
+});
