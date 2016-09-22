abac012f4d3efd3f94293873beae2e51b4ba833c
Emil Lengman <emillen@kth.se>
2016-09-22 16:59:09 +0200

    add factory for Category model

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: daseel
- * Date: 2016-09-18
- * Time: 23:04
- */
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+
+    return [
+        'to' => $faker->name
+
+    ];
+});
