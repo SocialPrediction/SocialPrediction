b77a94c49360e3b0670a9e69f2d2dc92ce29b2da
Emil Lengman <emillen@kth.se>
2016-09-23 12:24:25 +0200

    create setUpMutedUsers() for testing mutedUsers()

--- tests/Models/UserTest.php
+++ tests/Models/UserTest.php
@@ -10,23 +10,46 @@ class UserTest extends TestCase
 {
     use DatabaseMigrations;
 
+    private $userCount;
+
     public function setup()
     {
+
         parent::setUp();
+        $this->userCount = 0;
         $this->setUpBaseusers();
+        $this->setUpMutedUsers();
     }
 
 
     private function setUpBaseusers()
     {
+        $this->userCount += 10;
         factory(User::class, 10)->create();
+
+        $this->userCount += 1;
         factory(User::class)->create([
             'name' => 'Abigail',
         ]);
+
+        $this->userCount += 10;
         factory(User::class, 10)->create();
     }
 
+    private function setUpMutedUsers()
+    {
+        $this->userCount += 10;
+        factory(User::class, 10)->create()->each(function ($u) {
 
+            $this->userCount++;
+            $u2 = factory(User::class)->create();
+
+            factory(UserMute::class)->create([
+                'blocker' => $u->id,
+                'blockee' => $u2->id
+            ]);
+        });
+    }
 
     /**
      * A basic functional test example.
@@ -37,6 +60,6 @@ class UserTest extends TestCase
     {
 
         $this->assertTrue(User::find(11)->name == 'Abigail');
-        $this->assertTrue(User::all()->count() == 21);
+        $this->assertTrue(User::all()->count() == $this->userCount);
     }
 }
